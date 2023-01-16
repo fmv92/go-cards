0805875ea418130fc2bc0611368e7f0cab64638f
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-16 15:03:00 -0600

    New deck function

--- deck.go
+++ deck.go
@@ -6,6 +6,18 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+	return cards
+}
+
 func (cards deck) print() {
 	for i, card := range cards {
 		fmt.Println(i, card)
