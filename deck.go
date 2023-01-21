b7ef849dafc7686d9076f40718bc50fcdc2a31d3
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-21 16:26:39 -0600

    Implemented readFromFile and error handling

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -34,9 +35,19 @@ func deal(d deck, handSize int) (deck, deck) {
 }
 
 func (d deck) toString() string{
-	return strings.Join([]string(d), ", ")
+	return strings.Join([]string(d), ",")
 }
 
 func (d deck)saveToFile(fileName string) error{
 	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	slice := strings.Split(string(bs), ",")
+	return deck(slice)
 }
--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	fmt.Println(cards.toString())
-	cards.saveToFile("Cards")
+	//cards := newDeck()
+	//fmt.Println(cards.toString())
+	//cards.saveToFile("my_cards")
+	newDeck := newDeckFromFile("my_cards")
+	newDeck.print()
+	//fmt.Println(newDeck.toString())
 }
