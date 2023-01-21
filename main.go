bf6afb66dc9b1297d968f369728a755b0c877be3
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-21 16:49:46 -0600

    Implemented shuffle function and RNG

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 func main() {
-	//cards := newDeck()
-	//fmt.Println(cards.toString())
-	//cards.saveToFile("my_cards")
-	newDeck := newDeckFromFile("my_cards")
-	newDeck.print()
-	//fmt.Println(newDeck.toString())
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
