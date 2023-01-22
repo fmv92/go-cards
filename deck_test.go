3086ee16585d5fdc9ff8ec504699cebb2df7a3ab
Fernando <fernando.mata.villalobos@gmail.com>
2023-01-21 18:06:44 -0600

    Added test for writing and reading from a file

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,18 @@ func TestNewDeck(t *testing.T) {
 	if d[deckLength-1] != "King of Clubs" {
 		t.Errorf("Expected King of Diamonds, but got %v", d[deckLength-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+	os.Remove("_decktesting")
 }
